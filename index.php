62e6a4a61b60c1eada66c9d0db82619d2ada14cf
Kristaps Zakis <kristaps.zakis@vaimo.com>
2021-11-23 07:59:50 +0200

    Implemented icon fonr. Mobile styling close to done

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Magebit homework</title>
+    <title>Magebit homework || Kristaps Zaķis</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14"></script>
     <link rel="stylesheet" type="text/css" media="all" href="assets/css/normalize.css" />
@@ -11,8 +11,8 @@
 <body>
     <header class="main-header">
         <a href="#" id="main-logo" title="Home">
-            <img src="assets/images/logo.svg" alt="Pineapple Logo" />
-            <span>pineapple</span>
+            <img src="assets/images/pineapple-logo.svg" alt="Pineapple Logo image" class="logo image"/>
+            <img src="assets/images/logo-text.svg" alt="Pineapple Logo text" class="logo text" />
         </a>
         <nav class="main-menu">
             <ul class="menu">
@@ -24,19 +24,28 @@
     </header>
 
     <main class="main-content">
-        <h1>Subscribe to newsletter</h1>
-        <div class="text">Subscribe to our newsletter and get 10% discount on pineapple glasses.</div>
+        <div class="form-content">
+            <h1>Subscribe to newsletter</h1>
+            <div class="text">Subscribe to our newsletter and get 10% discount on pineapple glasses.</div>
 
-        <form>
-            <input type="text" placeholder="Type your email address here…" />
+            <form class="subscribe-form">
+                <div class="control">
+                    <input type="text" class="email" placeholder="Type your email address here…" />
+                    <button type="submit" value="subscribe" class="action submit"></button>
+                </div>
 
-            <div class="field checkbox">
-                <input type="checkbox" id="newsletter-agree" name="newsletter-agree" />
-                <label for="newsletter-agree">
-                    I agree to <a href="#">Terms of service</a>
-                </label>
-            </div>
-        </form>
+                <div class="field checkbox">
+                    <input type="checkbox" id="newsletter-agree" name="newsletter-agree" />
+                    <label for="newsletter-agree" class="label">
+                        I agree to <a href="#">Terms of service</a>
+                    </label>
+                </div>
+            </form>
+        </div>
+
+        <div class="success-content" style="display: none;">
+
+        </div>
 
         <footer>
             <ul>
